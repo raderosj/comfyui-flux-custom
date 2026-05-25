aa64de60db8347464713e6411ae0f6a7e670c1ea
raderosj <raderosj@gmail.com>
2026-05-25 17:55:58 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,15 +8,15 @@ mkdir -p /ComfyUI/models/loras
 
 pip install -q huggingface_hub hf_transfer
 
-export HF_HUB_ENABLE_HF_TRANSFER=1
+export HF_XET_HIGH_PERFORMANCE=1
 
 echo "📥 Скачиваем модели Flux..."
-huggingface-cli download raderos/comfyui-models-flux \
+hf download raderos/comfyui-models-flux \
   --local-dir /ComfyUI/models \
   --token ${HF_TOKEN}
 
 echo "📥 Скачиваем модели Qwen..."
-huggingface-cli download raderos/comfyui-models-qwen \
+hf download raderos/comfyui-models-qwen \
   --local-dir /ComfyUI/models \
   --token ${HF_TOKEN}
 
