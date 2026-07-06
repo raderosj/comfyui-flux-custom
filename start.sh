539af1e68af247bcf4d573a1d151e0ed498a49d8
raderosj <raderosj@gmail.com>
2026-07-06 09:45:07 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -11,7 +11,7 @@ mkdir -p /ComfyUI/models/insightface/models
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 export HF_TOKEN=${HF_TOKEN}
 
-# используем hf_transfer только для Flux
+# Используем hf_transfer
 export HF_HUB_ENABLE_HF_TRANSFER=1
 
 echo "========================================"
@@ -22,12 +22,15 @@ echo "huggingface_hub:"
 python3 -c "import huggingface_hub; print(huggingface_hub.__version__)"
 
 echo "========================================"
-echo "Скачиваем Flux..."
+echo "Скачиваем модели Flux..."
 echo "========================================"
 
 python3 << 'EOF'
 from huggingface_hub import snapshot_download
 import os
+import time
+
+t0 = time.time()
 
 snapshot_download(
     repo_id="raderos/comfyui-models-flux",
@@ -37,72 +40,7 @@ snapshot_download(
     max_workers=2,
 )
 
-print("Flux готов.")
-EOF
-
-########################################
-# ДЛЯ QWEN НЕ ИСПОЛЬЗУЕМ SNAPSHOT_DOWNLOAD
-########################################
-
-export HF_HUB_ENABLE_HF_TRANSFER=0
-
-echo ""
-echo "========================================"
-echo "Скачиваем Qwen по файлам..."
-echo "========================================"
-
-python3 << 'EOF'
-from huggingface_hub import hf_hub_download
-import shutil
-import os
-
-repo = "raderos/comfyui-models-qwen"
-token = os.environ.get("HF_TOKEN")
-
-files = [
-
-("diffusion_models/Qwen-Image-Edit-2509_fp8_e4m3fn.safetensors",
-"/ComfyUI/models/diffusion_models/Qwen-Image-Edit-2509_fp8_e4m3fn.safetensors"),
-
-("loras/Qwen-Image-Edit-2509-Lightning-4steps-V1.0-bf16.safetensors",
-"/ComfyUI/models/loras/Qwen-Image-Edit-2509-Lightning-4steps-V1.0-bf16.safetensors"),
-
-("loras/next-scene_lora-v2-3000.safetensors",
-"/ComfyUI/models/loras/next-scene_lora-v2-3000.safetensors"),
-
-("loras/qwen_multiple_angles_lora.safetensors",
-"/ComfyUI/models/loras/qwen_multiple_angles_lora.safetensors"),
-
-("text_encoders/Qwen2.5-VL-7B-Instruct-mmproj-bf16.gguf",
-"/ComfyUI/models/text_encoders/Qwen2.5-VL-7B-Instruct-mmproj-bf16.gguf"),
-
-("text_encoders/qwen_2.5_vl_7b_fp8_scaled.safetensors",
-"/ComfyUI/models/text_encoders/qwen_2.5_vl_7b_fp8_scaled.safetensors"),
-
-("vae/qwen_image_vae.safetensors",
-"/ComfyUI/models/vae/qwen_image_vae.safetensors"),
-
-]
-
-for remote, local in files:
-
-    if os.path.exists(local):
-        print("Есть:", os.path.basename(local))
-        continue
-
-    print("Скачиваем:", remote)
-
-    tmp = hf_hub_download(
-        repo_id=repo,
-        filename=remote,
-        token=token,
-    )
-
-    shutil.copy2(tmp, local)
-
-    print("Готово:", os.path.basename(local))
-
-print("Qwen готов.")
+print(f"Flux готов. ({time.time()-t0:.1f} сек)")
 EOF
 
 echo ""
